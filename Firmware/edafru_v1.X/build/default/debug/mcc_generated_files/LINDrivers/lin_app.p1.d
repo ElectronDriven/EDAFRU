5089de48c805bc5277c7ed86520259d43ae4ff88
electrondriven <ma@electrondriven.com>
2026-06-10 06:41:14 -0400

    the project has been planned.

---
+++ Firmware/edafru_v1.X/build/default/debug/mcc_generated_files/LINDrivers/lin_app.p1.d
@@ -0,0 +1,4 @@
+build/default/debug/mcc_generated_files/LINDrivers/lin_app.p1:  \
+mcc_generated_files/LINDrivers/lin_app.c  \
+mcc_generated_files/LINDrivers/lin_app.h  \
+mcc_generated_files/LINDrivers/lin_master.h 
---
+++ Firmware/edafru_v1.X/build/default/debug/mcc_generated_files/LINDrivers/lin_master.p1.d
@@ -0,0 +1,5 @@
+build/default/debug/mcc_generated_files/LINDrivers/lin_master.p1:  \
+mcc_generated_files/LINDrivers/lin_master.c  \
+mcc_generated_files/LINDrivers/lin_master.h  \
+mcc_generated_files/LINDrivers/../eusart1.h  \
+mcc_generated_files/LINDrivers/../tmr0.h 
