beb509687bdce6deb332f6535d476a03261c24b7
electrondriven <ma@electrondriven.com>
2026-06-17 03:15:49 -0400

    the hardware is developing..

--- Firmware/edafru_v1.X/build/default/debug/mcc_generated_files/LINDrivers/lin_app.p1.d
+++
@@ -1,4 +0,0 @@
-build/default/debug/mcc_generated_files/LINDrivers/lin_app.p1:  \
-mcc_generated_files/LINDrivers/lin_app.c  \
-mcc_generated_files/LINDrivers/lin_app.h  \
-mcc_generated_files/LINDrivers/lin_master.h 
